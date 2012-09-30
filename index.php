7fdaae2fcf2a0a64d295aa5da43c3ebb16a5ebbe
Aaron Parecki <aaron@parecki.com>
2012-09-30 10:22:08 -0700

    Supports jsonp callbacks

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include('inc.php');
 
-if(preg_match('|^/timezone/([0-9\.-]+)/([0-9\.-]+)$|', $_SERVER['REQUEST_URI'], $match) || (get('latitude') && get('longitude'))) {
+if(preg_match('|^/timezone/([0-9\.-]+)/([0-9\.-]+)|', $_SERVER['REQUEST_URI'], $match) || (get('latitude') && get('longitude'))) {
 	$db = new PDO(PDO_DSN, PDO_USER, PDO_PASS, array(PDO::ATTR_PERSISTENT => TRUE));
 
 	$latitude = get('latitude') ?: $match[1];
@@ -9,29 +9,37 @@ if(preg_match('|^/timezone/([0-9\.-]+)/([0-9\.-]+)$|', $_SERVER['REQUEST_URI'], 
 
 	$timezone = timezoneFromLocation($latitude, $longitude);
 
-	header('Content-type: application/json');
-
 	if($timezone) {
 		$tz = new DateTimeZone($timezone);
 		$now = new DateTime(date('c'));
 		$now->setTimeZone($tz);
 	
-		echo json_encode(array(
+		respond(array(
 			'timezone' => $timezone,
 			'offset' => $now->format('P'),
 			'seconds' => (int)$now->format('Z')
 		));
 	} else {
-		echo json_encode(array(
+		respond(array(
 			'error' => 'no_data'
 		));
 	}
 
 } else {
 	header('HTTP/1.1 404 Not Found');
-	header('Content-type: application/json');
-	echo json_encode(array(
+	respond(array(
 		'error' => 'not_found'
 	));
 }
 
+function respond($data) {
+	header('Content-type: application/json');
+	if(preg_match('/callback=([^\&]+)/', $_SERVER['REQUEST_URI'], $match) || get('callback')) {
+		$cb = get('callback') ?: $match[1];
+		echo $cb . '(';
+		echo json_encode($data);
+		echo ')';
+	} else {
+		echo json_encode($data);
+	}
+}
